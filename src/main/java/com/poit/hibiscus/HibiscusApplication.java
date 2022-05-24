25768eb4ad0101dd9f5ea9fc3a621f544d283a8f
Nikita Lobanovsky <nikitalobanovsky1@gmail.com>
2022-05-24 14:37:55 +0300

    added props

--- src/main/java/com/poit/hibiscus/HibiscusApplication.java
+++ src/main/java/com/poit/hibiscus/HibiscusApplication.java
@@ -1,9 +1,12 @@
 package com.poit.hibiscus;
 
+import com.poit.hibiscus.config.ClientUtilitiesProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ClientUtilitiesProperties.class)
 public class HibiscusApplication {
 
 	public static void main(String[] args) {
